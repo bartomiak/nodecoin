f312327247d8330028db76c34b786b6fb1c4a3d6
Bartek Tomiak <bartomiak@gmail.com>
2020-11-30 19:24:15 +0100

    check if chain is valif

--- main.js
+++ main.js
@@ -32,9 +32,35 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i]
+            const previousBlock = this.chain[i - 1]
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
 }
 
 let nodecoin = new Blockchain()
-nodecoin.addBlock(new Block(0, 'eloszka'))
+nodecoin.addBlock(new Block(0, { amount: 5 }))
+nodecoin.addBlock(new Block(1, { amount: 55 }))
 
 console.log(nodecoin)
+console.log(`Is valid?`, nodecoin.isChainValid())
+
+nodecoin.chain[1].data = { amount: 6 }
+console.log(`Is valid?`, nodecoin.isChainValid())
+
+nodecoin.chain[1].data = { amount: 50 }
+console.log(`Is valid?`, nodecoin.isChainValid())
