4c5e8030b00bbe97219de9319e3b7f7eb3333581
Bartek Tomiak <btomiak@user.com>
2020-11-30 18:37:03 +0100

    methods

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
 const SHA256 = require('crypto-js/sha256')
 
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
+    constructor(index, data, previousHash = '') {
         this.index = index;
-        this.timestamp = timestamp;
+        this.timestamp = new Date();
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
 
 class Blockchain {
     constructor() {
-        this.chain = [this.genesisBlock()]
+        this.chain = [this.genesisBlock()];
     }
 
     genesisBlock() {
-        return new Block(0, new Date(), 'genesis', '0')
+        return new Block(0, 'genesis', '0');
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
     }
 }
+
+let nodecoin = new Blockchain()
+nodecoin.addBlock(new Block(0, 'eloszka'))
+nodecoin.addBlock(new Block(1, 'dupa'))
+nodecoin.addBlock(new Block(1, 'dupa'))
+nodecoin.addBlock(new Block(1, 'dupa'))
+nodecoin.addBlock(new Block(1, 'dupa'))
+nodecoin.addBlock(new Block(1, 'dupa'))
+nodecoin.addBlock(new Block(1, 'dupa'))
+nodecoin.addBlock(new Block(1, 'dupa'))
+nodecoin.addBlock(new Block(1, 'dupa'))
+nodecoin.addBlock(new Block(1, 'dupa'))
+nodecoin.addBlock(new Block(1, 'dupa'))
+nodecoin.addBlock(new Block(1, 'dupa'))
+
+console.log(nodecoin)
